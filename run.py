e64cab0707e013664a9a4617f53e9185808fdc83
admin <admin@MIS-MAC-SRV01.local>
2026-07-14 15:30:05 +1000

    updated gitignore

--- run.py
+++
@@ -1,6 +0,0 @@
-from waitress import serve
-from app import app 
-
-if __name__ == '__main__':
-    print("Starting production server on http://0.0.0.0:6767...")
-    serve(app, host='0.0.0.0', port=6767)
